7dc83c2405d2e85e61969fcaf2f1e415249ae428
mohammedbadusha <mohammedbadusha211@gmail.com>
2022-12-09 23:31:52 +0530

    first commit

---
+++ src/main/java/com/simplilearn/phase3/BuyRepo.java
@@ -0,0 +1,7 @@
+package com.simplilearn.phase3;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BuyRepo extends JpaRepository<Buy, Integer>{
+
+}
---
+++ src/main/java/com/simplilearn/phase3/CartRepo.java
@@ -0,0 +1,7 @@
+package com.simplilearn.phase3;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CartRepo extends JpaRepository<Cart, Integer>{
+
+}
